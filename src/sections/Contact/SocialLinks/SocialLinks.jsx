b9cb173e341d040aa36166a465056b5e23641abc
manas kumar <66530122+supermanas@users.noreply.github.com>
2021-08-28 10:10:34 +0530

    Update SocialLinks.jsx

--- src/sections/Contact/SocialLinks/SocialLinks.jsx
+++ src/sections/Contact/SocialLinks/SocialLinks.jsx
@@ -4,23 +4,23 @@ import classes from './social.module.scss';
 export default function SocialLinks() {
   return (
     <div className={classes.social}>
-      <a href='mailto: akrishnamoorthy007@gmail.com' className='icon'>
+      <a href='mailto: kumaraditiya661@gmail.com' className='icon'>
         &#xe90c;
       </a>
-      <a href='https://github.com/KrishnaMoorthy12' className='icon'>
+      <a href='https://github.com/supermanas' className='icon'>
         &#xe908;
       </a>
-      <a href='https://twitter.com/krish_the_dev' className='icon'>
+      <a href='#' className='icon'>
         &#xe90e;
       </a>
-      <a href='https://linkedin.com/in/akrishnamoorthy007' className='icon'>
+      <a href='#' className='icon'>
         &#xe907;
       </a>
-      <a href='https://fb.com/akrishnamoorthy007' className='icon'>
+      <a href='#' className='icon'>
         &#xe90d;
       </a>
       <a
-        href='https://join.slack.com/t/akrishnamoorthy007/shared_invite/zt-h38nw07o-iwbECk2CYVTv0pNokG884g'
+        href='#'
         className='icon'
       >
         &#xe902;
