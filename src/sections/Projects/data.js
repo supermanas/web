500bca7d7c78ec5d693eba694cf87abd93f243e0
manas kumar <66530122+supermanas@users.noreply.github.com>
2021-08-28 09:23:24 +0530

    Update data.js

--- src/sections/Projects/data.js
+++ src/sections/Projects/data.js
@@ -4,7 +4,7 @@ import { AubitLogo, AubitMobile } from '../../images/Aubit';
 import { InfiniteLoopClub, InfiniteLoopClubLogo } from '../../images/InfiniteLoopClub';
 
 const collegemate = {
-  title: 'Collegemate',
+  title: 'Createch 3.0',
   type: 'Featured project',
   logo: CollegemateLogo,
   images: {
@@ -12,22 +12,22 @@ const collegemate = {
     dark: { small: CollegemateDark, large: CollegemateDark }
   },
   description:
-    'A MERN stack Progressive Web App (PWA) built for students and teaching faculties to easily share materials, homework, assignments, and notes.',
+    'AN annual tech event hosted by createch club in view to widespread knowledge',
   features: [
-    'Authentication system',
-    'Email alerts',
-    'File sharing',
-    'Creating new posts and materials',
-    'Scheduling based on current time',
-    'Dark theme'
+    'assured 60lac prizes',
+    'online event',
+    'free regisration',
+    'no limits',
+    'cool and exciting rewards',
+    'ryan vk'
   ],
-  techStack: ['React', 'Express', 'MongoDB', 'PWA'],
+  techStack: ['java', 'css', 'html', 'wordpress'],
   link: 'https://createchclub.xyz/',
   repo: undefined
 };
 
 const ilc = {
-  title: 'Infinite Loop Club website',
+  title: 'createch website',
   type: 'Collaborated project',
   logo: InfiniteLoopClubLogo,
   images: {
@@ -35,11 +35,11 @@ const ilc = {
     dark: { small: InfiniteLoopClub, large: InfiniteLoopClub }
   },
   description:
-    'The official website of the Infinite Loop Club of Anna University, Trichy. I have worked on both frontend and backend of this website and also designed most of the elements.',
-  features: ['Registration', 'Online ID Generation', 'Post sharing', 'Online polling system'],
-  techStack: ['React JS', 'Express JS', 'Mongo DB'],
-  link: 'https://infinite-loop-club.netlify.app/',
-  repo: 'https://github.com/Infinite-Loop-Club/club-site'
+    'The official website of the createch club 2021-22',
+  features: ['200+members', 'build with efforts', 'learning sessions', 'widespread knowledge'],
+  techStack: ['java', 'html', 'css'],
+  link: 'https://createchclub.xyz',
+  repo: undefined
 };
 
 const $7d50p = {
@@ -51,15 +51,15 @@ const $7d50p = {
     dark: { small: SFPImageSmall, large: SFPImageLarge }
   },
   description:
-    'A fun challenge, to complete a collection of 50 projects within 7 days, inspired from Brad Traversy and Florin Pop’s ‘50 Projects 50 Days’. Completed 20 projects and made contributions to 5+ other projects. Some projects I really like, that I made:',
+    'A fun challenge, to complete a collection of 50 projects within 7 days.',
   features: ['Theme clock', 'Testimonials', 'Movie app', 'Double click like'],
-  techStack: ['Next JS', 'styled-components', 'TypeScript'],
+  techStack: ['JS', 'styled-components', 'TypeScript'],
   link: 'https://7days50projects.vercel.app/',
   repo: 'https://github.com/benxene/7days-50projects'
 };
 
 const aubit = {
-  title: 'AU-BIT Unofficial website',
+  title: 'createch 2.0 website',
   type: 'Collaborated project',
   logo: AubitLogo,
   images: {
@@ -67,11 +67,11 @@ const aubit = {
     dark: { small: AubitMobile, large: AubitMobile }
   },
   description:
-    'The unofficial website of University College of Engineering, BIT Campus, Tiruchirappalli. The website is still under development and will be approved upon completion to be the official website. Built with React.',
-  features: ['Modern design', 'Online Form downloads', 'Reusable web components', 'Image Gallery'],
-  techStack: ['React', 'SCSS'],
-  link: 'https://aubit.netlify.app/',
-  repo: undefined
+    'The official website of createch 2.0.',
+  features: ['online learning', 'free sessions ', '2 days 10 event', 'ultimate sessions'],
+  techStack: ['robotics', 'web dev'],
+  link: 'http://www.createchrisvk.in/',
+   repo: 'https://github.com/hridizaroy/createch'
 };
 
 const data = [collegemate, ilc, aubit, $7d50p];
